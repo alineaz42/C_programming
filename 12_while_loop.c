edd9465d5a4a2370f21c1c582b7ebdcd0686e35a
Ali Neaz <neazali77@gmail.com>
2022-02-16 13:40:47 +0600

    for loop done

--- 12_while_loop.c
+++ 12_while_loop.c
@@ -1,10 +1,24 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 0;
+//     while (i <= 100)
+//     {
+//         printf("%d\n", i);
+//         i++;
+//     }
+// }
+
 #include <stdio.h>
 int main()
 {
-    int i = 0;
-    while (i <= 100)
+    int i = 0, num;
+    printf("Enter the num: ");
+    scanf("%d", &num);
+    while (i <= num)
     {
         printf("%d\n", i);
-        i++;
+        i = i + 1;
     }
+    return 0;
 }
---
+++ 15_for_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+// in expressio 1 in for loop you can initialize more than one variable
