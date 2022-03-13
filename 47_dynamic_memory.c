f2e8eebc2c8a416bac5a269c28dffc8888a63b66
Ali Neaz <neazali77@gmail.com>
2022-03-14 01:10:17 +0600

    did not complete this

--- 47_dynamic_memory.c
+++ 47_dynamic_memory.c
@@ -4,20 +4,35 @@
 int main()
 {
     // video 20min still in malloc
-    // use of malloc
-    int *ptr;
-    ptr = (int *)malloc(3 * sizeof(int));
-    printf("Enter ptrs: \n");
-
-    for (int i = 0; i < 3; i++)
+    // use of malloc // defaul values ar garbage
+    /*
+    int *ptr, n;
+    ptr = (int *)malloc(n * sizeof(int));
+    printf("Enter the size of the array you want to: \n");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
     {
-        // printf("%d\n", ptr + i);
-        scanf("%d", ptr[i]);
+        printf("Enter the value of %d of this array\n", i);
+        scanf("%d", &ptr[i]);
     }
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("The valaue at: %d is = %d\n", i, ptr[i]);
-        // scanf("%d", ptr[i]);
+        printf("i=%d; and ptr=%d\n", i, ptr[i]);
+    }
+    */
+    // use of calloc() // we use calloc cause defaults are 0
+    int *ptr, n;
+    ptr = (int *)calloc(n, sizeof(int));
+    printf("Enter the size of the array you want to: \n");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the value of %d of this array\n", i);
+        scanf("%d", &ptr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("i=%d; and ptr=%d\n", i, ptr[i]);
     }
     return 0;
 }
