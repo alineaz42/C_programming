3034d40656728a3160c88b5577a26fc5ea148df1
Ali Neaz <neazali77@gmail.com>
2022-03-25 01:27:44 +0600

    done with this 57tutorial

---
+++ 57_preprocessors.c
@@ -0,0 +1,16 @@
+// preprocessing,compiling,assembly,linking
+// preprocessing
+/*
+1. removal of comments
+2. expansion of macros
+3. expansion of include files
+// compiling
+1. assembly level instructions are generated
+// assembly
+.o or .exe files are generated
+fns like pritf are not resolved
+assembly level instructions are generated to machine codes
+// linking
+links the functions implementations
+*/
+// preprocessing further info: https://codewithharry.com/videos/c-language-tutorials-in-hindi-58
