d099041b9c951f397bbf91828b88433de4d7316e
Ali Neaz <neazali77@gmail.com>
2022-04-09 02:17:16 +0600

    cmd args done

--- 68_cmd_args.c
+++ 68_cmd_args.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char const *argv[])
 {
-
+    printf("The number of var: %d\n", argc);
+    for (int i = 0; i < argc; i++)
+    {
+        printf("The val at index %d is: %s\n", i, argv[i]);
+    }
     return 0;
 }
